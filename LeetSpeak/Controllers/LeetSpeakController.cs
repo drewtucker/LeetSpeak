7ef76b54d5b2dd744ccf80196b353b9d17b6dee8
Drew Tucker and Jahmanz Williams <drew+jahmanz@>
2018-02-13 16:30:58 -0800

    Add Views and Controllers, still working on displaying resulting sentence on page after form entry.

--- LeetSpeak/Controllers/LeetSpeakController.cs
+++ LeetSpeak/Controllers/LeetSpeakController.cs
@@ -1,11 +1,31 @@
-
 using Microsoft.AspNetCore.Mvc;
 using LeetSpeakApp.Models;
+using System;
+
 namespace LeetSpeakApp.Controllers
 {
     public class HomeController : Controller
     {
+      [Produces("text/html")]
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+      return View();
+      }
 
 
+      [HttpGet("/form")]
+      public ActionResult Form()
+      {
+        return View();
+      }
+
+      [HttpPost("/")]
+      public ActionResult Return()
+      {
+        string userSentence = (Request.Form["phrase"]);
+        string convertedSentence = LeetSpeak.LeetTranslate(userSentence);
+        return View("Index", convertedSentence);
+      }
     }
 }
--- LeetSpeak/Models/LeetSpeak.cs
+++ LeetSpeak/Models/LeetSpeak.cs
@@ -1,10 +1,21 @@
-using System;
 using System.Collections.Generic;
 
 namespace LeetSpeakApp.Models
 {
   public class LeetSpeak
   {
+    private string _translation;
+    public string translatedSentence = "";
+    public string GetTranslation()
+    {
+      return _translation;
+    }
+
+    public void SetTranslation(string translatedSentence)
+    {
+      _translation = translatedSentence;
+    }
+
     public static string LeetTranslate(string inputSentence)
     {
       string translatedSentence = inputSentence.Replace("e", "3").Replace("E", "3").Replace("A", "4").Replace("a", "4").Replace("t", "7").Replace("T", "7").Replace("s", "z").Replace("o", "0").Replace("O", "0");
